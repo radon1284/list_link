0835ffb2685200bf54a7245a166f5e19717ded3d
Prelang Builder <builder@prelang.com>
2016-04-02 08:40:10 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Link' to allow for create, update

--- app/admin/link.rb
+++ app/admin/link.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Link do
+  permit_params :link_name, :description, :url
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
