770df27eabe360e87804f498f3c12781645da103
Ruel Nopal <radon1284@yahoo.com>
2016-04-03 13:10:07 +0800

    bjsddhwrw

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -115,13 +115,13 @@ class LinksController < ApplicationController
   def upvote
     @link = Link.find(params[:id])
     @link.upvote_by current_user
-    redirect_to links_path
+    redirect_to links_path(@link)
   end
 
   def downvote
     @link = Link.find(params[:id])
     @link.downvote_by current_user
-    redirect_to links_path
+    redirect_to links_path(@link)
 end
 
 
